9aa6600bea557909bcb122a406e3798003464521
chinchillapsico <coraline.colous@gmail.com>
2017-05-04 23:56:19 -0300

    cambios

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+function MiembroSquad(nombre, edad, hobbie1, hobbie2, hobbie3){
+this.nombre=nombre;	
+this.edad= edad;
+this.hobbie1=hobbie1;
+this.hobbie2=hobbie2;
+this.hobbie3=hobbie3;
+}
+
+var int1 = new integrante("macarena","apellido", edad, "hobbie1", "hobbie2", "hobbie3");
+var int2 = new integrante("macarena","apellido", edad, "hobbie1", "hobbie2", "hobbie3");
+var int3 = new integrante("macarena","apellido", edad, "hobbie1", "hobbie2", "hobbie3");
+var int4 = new integrante("macarena","apellido", edad, "hobbie1", "hobbie2", "hobbie3");
+var int5 = new integrante("macarena","apellido", edad, "hobbie1", "hobbie2", "hobbie3");
+var int6 = new integrante("macarena","apellido", edad, "hobbie1", "hobbie2", "hobbie3");
+var int7 = new integrante("macarena","apellido", edad, "hobbie1", "hobbie2", "hobbie3");
+var int8 = new integrante("Andrea","Miranda", 24, "dibujar", "leer", "aprender cosas nuevas");
+
+function listado() {
+	var mostrar = //traer un elemento x id
+	var lista = "Nombre:" + nombre + "<br>" + "Edad:" + edad + "<br>"+ "Hobbies:"+ "<br>"+ hobbie1+ hobbie2+hobbie3
+}
+mostrar.listado();
+mostrar.innerHTML = lista;
